d8e33d3beecf9ffe7d0196aded0d435e789341e7
Aris Karagiannidis <arkaragian@gmail.com>
2021-11-18 00:01:04 +0200

    Solution: Add a test project.

---
+++ TestlibCSV/libCSVTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestlibCSV {
+    [TestClass]
+    public class libCSVTests {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
