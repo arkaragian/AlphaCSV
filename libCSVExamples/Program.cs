0058d703073957c8d2db66be6be5277024ada09b
Aris Karagiannidis <arkaragian@gmail.com>
2022-01-09 21:48:52 +0200

    Add a project to shocase sample usage.

---
+++ libCSVExamples/Program.cs
@@ -0,0 +1,39 @@
+﻿using libCSV;
+using System.Data;
+
+namespace libCSVExamples {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to libCSVExamples. Those examples showcase the use of the library.");
+
+            DataTable simple = ReadSimpleCSV("SampleCSV/TwoColumnCSV.csv");
+
+            PrintDataTable(simple);
+        }
+
+        public static DataTable ReadSimpleCSV(string filename) {
+            DataTable schema = new DataTable();
+            schema.Columns.Add(new DataColumn("ColumnA", typeof(string)));
+            schema.Columns.Add(new DataColumn("ColumnB", typeof(string)));
+
+            DataTable result = CSVParser.ReadDefinedCSV(schema, filename);
+            return result;
+        }
+
+
+        public static void PrintDataTable(DataTable table) {
+            //Print the header
+            foreach (DataColumn col in table.Columns) {
+                Console.Write($"{col.ColumnName}\t");
+            }
+            Console.Write("\n");
+            //Print the rows
+            foreach (DataRow r in table.Rows) {
+                foreach (var item in r.ItemArray) {
+                    Console.Write($"{item}\t");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
