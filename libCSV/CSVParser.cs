d54b26b7f5d1325b2d207c44007ca2cb2904fab2
Aris Karagiannidis <arkaragian@gmail.com>
2022-01-30 16:26:14 +0200

    CSVParser.cs: Create interface to help users with their unit testing. Class is no longer partial.

--- libCSV/CSVParser.cs
+++ libCSV/CSVParser.cs
@@ -5,12 +5,13 @@
 using System.Data;
 using System.Text.RegularExpressions;
 using System.IO.Abstractions;
+using libCSV.Interfaces;
 
 namespace libCSV {
     /// <summary>
     /// This is the primary class that is used in order to parse a CSV file.
     /// </summary>
-    public partial class CSVParser {
+    public class CSVParser : ICSVParser {
 
         readonly IFileSystem FSInterface;
 
@@ -26,6 +27,36 @@ namespace libCSV {
         /// The default constructor
         /// </summary>
         public CSVParser() : this(new FileSystem()) {
+        }
+
+        /// <summary>
+        /// Parses a CSV file without a given schema.
+        /// <remarks>This method will assume that all the fields are of string type.</remarks>
+        /// </summary>
+        /// <param name="path">The path of the file that will be read</param>
+        /// <param name="options">(Optional argument) The options of the file that will be read.</param>
+        /// <param name="validationPatterns">(Optional Argument) Validation patterns in terms of regular expressions</param>
+        /// <returns>The contents of a CSV file in the form of a datatable</returns>
+        public DataTable ParseSimpleCSV(string path, CSVParseOptions options = null, List<string> validationPatterns = null) {
+            //Before we continue we need to make some assumptions for the file. e.g to know how many fields we need to parse.
+            //Thus we read only the first line, deduce information there and try to move forward.
+
+            //File.ReadLines makes use of lazy evaluation and doesn't read the whole file into an array of lines first.
+            //https://stackoverflow.com/questions/27345854/read-only-first-line-from-a-text-file/27345927
+            string FirstLine = FSInterface.File.ReadLines(path).First();
+
+            if (options == null) {
+                options = new CSVParseOptions();
+            }
+
+            string[] fields = ParseLine(FirstLine, options);
+            DataTable schema = new DataTable();
+            foreach (string field in fields) {
+                DataColumn column = new DataColumn(field, typeof(string));
+                schema.Columns.Add(column);
+            }
+
+            return ParseDefinedCSV(schema, path, options, null);
         }
 
         /// <summary>
@@ -137,8 +168,7 @@ namespace libCSV {
         /// <param name="line">The line to be parsed</param>
         /// <param name="options">The parsing options</param>
         /// <returns>An array containing all the seperate fields that comprise the file.</returns>
-        //TODO: Make this method non static and private. This need refactoring to the unit tests. 
-        public static string[] ParseLine(string line, CSVParseOptions options) {
+        private string[] ParseLine(string line, CSVParseOptions options) {
             List<string> fields = new List<string>();
             //This is quite a simple implementation. We iterrate through each character and assign it to a field.
 
---
+++ libCSV/Interfaces/ICSVParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libCSV.Interfaces {
+    public interface ICSVParser {
+        DataTable ParseDefinedCSV(DataTable schema, string path, CSVParseOptions options = null, List<string> validationPatterns = null);
+        DataTable ParseSimpleCSV(string path, CSVParseOptions options = null, List<string> validationPatterns = null);
+    }
+}
