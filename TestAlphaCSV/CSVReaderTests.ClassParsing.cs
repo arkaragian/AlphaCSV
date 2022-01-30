7a63c806470c11f50062ee97774ad535bb5a0a62
Aris Karagiannidis <arkaragian@gmail.com>
2022-01-30 22:49:07 +0200

    CSVReaderTests.ClassParsing.cs: Add more tests.

--- TestAlphaCSV/CSVReaderTests.ClassParsing.cs
+++ TestAlphaCSV/CSVReaderTests.ClassParsing.cs
@@ -49,6 +49,33 @@ namespace TestAlphaCSV {
         }
 
         [TestMethod]
+        public void TestSimpleClassParsingReverseColumnOrder() {
+            string input = "Surname,Name\nDoe,John\n";
+
+            //Arrange
+            MockFileSystem fs = new MockFileSystem();
+            MockFileData mockInputFile = new MockFileData(input);
+            string path = @"C:\test.csv";
+            fs.AddFile(path, mockInputFile);
+
+            TestRecord rc = new TestRecord {
+                Name = "John",
+                Surname = "Doe"
+            };
+            List<TestRecord> expected = new List<TestRecord> { rc };
+
+
+            //Act
+            CSVParser parser = new CSVParser(fs); //Inject dependency here
+            //Since we have the expected result we just clone the schema instead of building it by hand.
+            List<TestRecord> actual = parser.ParseType<TestRecord>(path);
+
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void TestDerivedClass() {
             string input = "Name,Surname,FatherName\nJohn,Doe,Donald\n";
 
