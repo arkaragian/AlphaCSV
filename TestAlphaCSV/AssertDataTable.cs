9a52f6a4813d85f879fffcbb140ff48ea540e2b3
Aris Karagiannidis <arkaragian@gmail.com>
2022-01-30 17:02:20 +0200

    AssertDataTable.cs: Rename first parameter

--- TestAlphaCSV/AssertDataTable.cs
+++ TestAlphaCSV/AssertDataTable.cs
@@ -11,20 +11,20 @@ namespace TestAlphaCSV {
     public static class AssertDataTable {
         //https://stackoverflow.com/questions/18365915/assert-areequal-method-usage-in-checking-datatable-types
 
-        public static void AreEqual(DataTable DT1, DataTable DT2) {
-            if ((DT1 == null) && (DT2 == null)) {
+        public static void AreEqual(DataTable expected, DataTable DT2) {
+            if ((expected == null) && (DT2 == null)) {
                 return;
-            } else if ((DT1 != null) && (DT2 != null)) {
-                if ((DT1.Columns.Count == DT2.Columns.Count)) {
+            } else if ((expected != null) && (DT2 != null)) {
+                if ((expected.Columns.Count == DT2.Columns.Count)) {
                     //Check that the column types are the same
-                    for (int i = 0; i < DT1.Columns.Count; i++) {
-                        if (DT1.Columns[i].DataType != DT2.Columns[i].DataType) {
+                    for (int i = 0; i < expected.Columns.Count; i++) {
+                        if (expected.Columns[i].DataType != DT2.Columns[i].DataType) {
                             throw new Exception("The type of of columns is not the same in both tables!");
                         }
                     }
                     //Check that the headers are the same
-                    for (int i = 0; i < DT1.Columns.Count; i++) {
-                        if (DT1.Columns[i].ColumnName != DT2.Columns[i].ColumnName) {
+                    for (int i = 0; i < expected.Columns.Count; i++) {
+                        if (expected.Columns[i].ColumnName != DT2.Columns[i].ColumnName) {
                             throw new Exception("Column names do not match!");
                         }
                     }
@@ -33,10 +33,10 @@ namespace TestAlphaCSV {
                 }
 
                 //Up to this point we have verified the columns. Now we are going th verify the rows
-                if (DT1.Rows.Count == DT2.Rows.Count) {
-                    for (int i = 0; i < DT1.Columns.Count; i++) {
-                        for (int j = 0; j < DT1.Rows.Count; j++) {
-                            if (!DT1.Rows[j][i].Equals(DT2.Rows[j][i])) {
+                if (expected.Rows.Count == DT2.Rows.Count) {
+                    for (int i = 0; i < expected.Columns.Count; i++) {
+                        for (int j = 0; j < expected.Rows.Count; j++) {
+                            if (!expected.Rows[j][i].Equals(DT2.Rows[j][i])) {
                                 throw new Exception("Cells are not equal!");
                             }
                         }
