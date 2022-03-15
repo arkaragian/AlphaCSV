f876dcf93a2775cc12ac2c553e3c5f745a8bc085
Aris Karagiannidis <arkaragian@gmail.com>
2022-03-15 10:58:35 +0200

    feat(`CSVWriteOptions`): add `QuoteFieldsWithoutDelimeter` flag
    Add a flag to enforce quoting even when the fields do not contain
    a delimeter.

--- AlphaCSV/CSVWriteOptions.cs
+++ AlphaCSV/CSVWriteOptions.cs
@@ -10,9 +10,15 @@ namespace AlphaCSV {
         /// </summary>
         public bool WriteHeaders { get; set; }
 
+        /// <summary>
+        /// A Flag that forces field quotation even if the field does not contain the delimeter.
+        /// </summary>
+        public bool QuoteFieldsWithoutDelimeter { get; set; }
+
         public CSVWriteOptions() {
             Delimeter = ',';
             QuoteCharacter = '"';
+            QuoteFieldsWithoutDelimeter = false;
             WriteHeaders = true;
             DateTimeFormat = "dd-MMM-yyyy";
             DecimalSeperator = '.';
